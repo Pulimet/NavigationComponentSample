d1d8bfbbf5053dee5ecb9e69fcd5187b6a4a9e97
Alexey Korolev <korolev2009@gmail.com>
2019-07-24 08:13:42 +0300

    Add setDrawerListener() to show menu icon animation when Drawer opening or closing

--- app/src/main/java/il/co/ynet/navigationcomponentsample/MainActivity.kt
+++ app/src/main/java/il/co/ynet/navigationcomponentsample/MainActivity.kt
@@ -2,7 +2,10 @@ package il.co.ynet.navigationcomponentsample
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -15,22 +18,41 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController : NavController
+    private lateinit var menuDrawer : DrawerArrowDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setToolBarAndNavigation()
+    }
+
+    private fun setToolBarAndNavigation() {
         setSupportActionBar(toolbar)
 
         navController = findNavController(R.id.nav_host_fragment)
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-            R.id.nav_tools, R.id.nav_share, R.id.nav_send), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+            ), drawerLayout
+        )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        menuDrawer = toolbar.navigationIcon as DrawerArrowDrawable
+        setDrawerListener()
+    }
+
+    private fun setDrawerListener() {
+        drawerLayout.addDrawerListener(object : DrawerLayout.SimpleDrawerListener() {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                menuDrawer.progress = slideOffset
+            }
+        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
